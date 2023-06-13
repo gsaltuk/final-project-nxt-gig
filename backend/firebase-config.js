dc808f2a662c14e87a249c014e4ba312b2539ea0
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-06-13 14:46:14 +0100

    setup firebase config

---
+++ backend/firebase-config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBPXGMJW3FEUfHfs_YuKeVnoQdS087B4z4",
+  authDomain: "nxt-gig.firebaseapp.com",
+  projectId: "nxt-gig",
+  storageBucket: "nxt-gig.appspot.com",
+  messagingSenderId: "522375441694",
+  appId: "1:522375441694:web:0deb39d89c58cc3e6c2d0f",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const firestore = firebase.firestore();
