978f2323f67631c1805ea6aa94deb297edd2683c
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-06-13 09:14:31 +0100

    firebase config initialised

---
+++ backend/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBPXGMJW3FEUfHfs_YuKeVnoQdS087B4z4",
+    authDomain: "nxt-gig.firebaseapp.com",
+    projectId: "nxt-gig",
+    storageBucket: "nxt-gig.appspot.com",
+    messagingSenderId: "522375441694",
+    appId: "1:522375441694:web:0deb39d89c58cc3e6c2d0f"
+  };
+
+  initializeApp(firebaseConfig);
+
+  
