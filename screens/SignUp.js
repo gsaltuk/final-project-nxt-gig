5009f56cd353d8f4c04b7c6b99f5adede47d8d03
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-06-13 16:30:52 +0100

    sign up form created and linked to firebase

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,7 +1,48 @@
-import { Text } from "react-native"
+import React, { useState } from "react";
+import { Text, View, TextInput, Button } from "react-native";
+import styles from "../styles/styles";
+import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth'
+import { firebase } from "../backend/firebase-config";
 
 export default function SignUpForm({ navigation }) {
-return (
-    <Text>Sign Up Form</Text>
-)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (text) => {
+    setEmail(text);
+  };
+
+  const handlePasswordChange = (text) => {
+    setPassword(text);
+  };
+
+  const handleSignup = async () => {
+    try {
+      const authInstance = getAuth(); // Initialize the authentication instance
+      await createUserWithEmailAndPassword(authInstance, email, password);
+      navigation.navigate("SetupProfile");
+    } catch (error) {
+      console.log("Signup Error:", error);
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={handleEmailChange}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={handlePasswordChange}
+        secureTextEntry
+      />
+      <Button title="Signup" onPress={handleSignup} />
+    </View>
+  );
 }
+
