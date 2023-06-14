978c3dba5424d08e783e4400baad326c6a46b6d6
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-06-14 16:44:52 +0100

    resolve merge conflicts on App.js

--- App.js
+++ App.js
@@ -1,62 +1,53 @@
-import * as React from 'react'
-import { useState } from 'react';
+import * as React from "react";
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import UserContext from './context/user-context';
+import UserContext from "./context/user-context";
 
-import Welcome from './screens/Welcome';
-import LoginForm from './screens/Login';
-import SignUpForm from './screens/SignUp';
-import SetupProfile from './screens/SetupProfile';
-import Home from './screens/Home'
-import NavBar from './screens/NavBar';
-import Artists from './screens/Artists';
-import Messages from './screens/Messages';
-import Profile from './screens/Profile';
-import SingleGig from './screens/SingleGig'
-import EditProfile from './screens/EditProfile';
+import Welcome from "./screens/Welcome";
+import LoginForm from "./screens/Login";
+import SignUpForm from "./screens/SignUp";
+import SetupProfile from "./screens/SetupProfile";
+import Home from "./screens/Home";
+import NavBar from "./screens/NavBar";
+import Artists from "./screens/Artists";
+import Messages from "./screens/Messages";
+import Profile from "./screens/Profile";
+import SingleGig from "./screens/SingleGig";
+import EditProfile from "./screens/EditProfile";
 
-
-
-const Stack = createNativeStackNavigator()
-
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-
-  
-  return (
-   <NavigationContainer>
-   <Stack.Navigator>
-      <Stack.Screen options={{headerShown: false}} name="Home" component={NavBar}></Stack.Screen>
-      <Stack.Screen name="Welcome" component={Welcome}></Stack.Screen>
-      <Stack.Screen name="SingleGig" component={SingleGig}></Stack.Screen>
-      <Stack.Screen name="Login" component={LoginForm}></Stack.Screen>
-      <Stack.Screen name="Signup-Form" component={SignUpForm}></Stack.Screen>
-      <Stack.Screen name="SetupProfile" component={SetupProfile}></Stack.Screen>
-      <Stack.Screen name="Artists" component={Artists}></Stack.Screen>
-      <Stack.Screen name="Messages" component={Messages}></Stack.Screen>
-      <Stack.Screen name="Profile" component={Profile}></Stack.Screen>
-    </Stack.Navigator>
-
-  const [user, setUser] = useState("")
+  const [user, setUser] = useState("");
 
   return (
-    <UserContext.Provider value={{user, setUser}}>
-    <NavigationContainer>
-   <Stack.Navigator>
-    <Stack.Screen name="SetupProfile" component={SetupProfile}></Stack.Screen>
-    <Stack.Screen name="Welcome" component={Welcome}></Stack.Screen>
-    <Stack.Screen name="Signup Form" component={SignUpForm}></Stack.Screen>
-   <Stack.Screen name="Profile" component={Profile}></Stack.Screen>
-    <Stack.Screen name="EditProfile" component={EditProfile}></Stack.Screen>
-    <Stack.Screen name="Login" component={LoginForm}></Stack.Screen>
-   <Stack.Screen name="Home" component={Home}></Stack.Screen>
-   </Stack.Navigator>
-
-    </NavigationContainer>
+    <UserContext.Provider value={{ user, setUser }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Home"
+            component={NavBar}
+          ></Stack.Screen>
+          <Stack.Screen name="Welcome" component={Welcome}></Stack.Screen>
+          <Stack.Screen name="SingleGig" component={SingleGig}></Stack.Screen>
+          <Stack.Screen name="Login" component={LoginForm}></Stack.Screen>
+          <Stack.Screen
+            name="Signup-Form"
+            component={SignUpForm}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="SetupProfile"
+            component={SetupProfile}
+          ></Stack.Screen>
+          <Stack.Screen name="Artists" component={Artists}></Stack.Screen>
+          <Stack.Screen name="Messages" component={Messages}></Stack.Screen>
+          <Stack.Screen name="Profile" component={Profile}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </UserContext.Provider>
   );
 }
