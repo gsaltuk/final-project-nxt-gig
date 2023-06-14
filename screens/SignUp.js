17b634d30997551461e800ffa781eb8b77622360
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-06-14 10:38:07 +0100

    add styling to signup page

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,18 +1,15 @@
 import React, { useState, useContext } from "react";
 import { Text, View, TextInput, Button } from "react-native";
 import styles from "../styles/styles";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-} from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { firebase } from "../backend/firebase-config";
 import UserContext from "../context/user-context";
-
+import { TouchableOpacity, KeyboardAvoidingView } from "react-native";
 
 export default function SignUpForm({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const {user, setUser} = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext);
 
   const handleEmailChange = (text) => {
     setEmail(text);
@@ -37,21 +34,25 @@ export default function SignUpForm({ navigation }) {
   };
 
   return (
-    <View>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={handleEmailChange}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={handlePasswordChange}
-        secureTextEntry
-      />
-      <Button title="Signup" onPress={handleSignup} />
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={handleEmailChange}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={handlePasswordChange}
+          secureTextEntry
+        />
+        <TouchableOpacity onPress={handleSignup} style={styles.button}>
+          <Text style={styles.button}>SIGN UP</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
