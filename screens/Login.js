54b2af7344e7f17d3e2dcec86f4bb26feed9c178
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-06-21 11:45:06 +0100

    styling implemented on welcome, login, singup, setup

--- screens/Login.js
+++ screens/Login.js
@@ -4,12 +4,12 @@ import {
   TouchableOpacity,
   View,
   KeyboardAvoidingView,
+  StyleSheet,
 } from "react-native";
 import React, { useState, useEffect, useContext } from "react";
 import UserContext from "../context/user-context";
-import styles from "../styles/styles";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import {firebase} from '../backend/firebase-config'
+import { firebase } from "../backend/firebase-config";
 
 export default function LoginForm({ navigation }) {
   const { setUser, setCurrentUid } = useContext(UserContext);
@@ -31,7 +31,7 @@ export default function LoginForm({ navigation }) {
       .then((result) => {
         // If we need user information such as email, photo url or number ... we can use result.user.
         setUser(result.user);
-        setCurrentUid(result.user.uid)
+        setCurrentUid(result.user.uid);
         navigation.navigate("Home");
       })
       .catch((error) => {
@@ -45,16 +45,16 @@ export default function LoginForm({ navigation }) {
   };
 
   return (
-    <KeyboardAvoidingView style={styles.container}>
+    <KeyboardAvoidingView style={styles.loginContainer}>
       <View style={styles.inputContainer}>
         <TextInput
-          placeholder="Email..."
+          placeholder="Email"
           //value=''
           onChangeText={(text) => setEmail(text)}
           style={styles.input}
         />
         <TextInput
-          placeholder="Password..."
+          placeholder="Password"
           //value=''
 
           secureTextEntry
@@ -65,9 +65,49 @@ export default function LoginForm({ navigation }) {
 
       <View style={styles.buttonContainer}>
         <TouchableOpacity onPress={handleSignIn} style={styles.button}>
-          <Text style={styles.button}>Login</Text>
+          <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  loginContainer: {
+    flex: 1,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 22,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#ffffff",
+    paddingHorizontal: 75,
+    paddingVertical: 7,
+    borderRadius: 5,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 18,
+    width: 120,
+    textAlign: "center",
+  },
+  input: {
+    backgroundColor: "#EDEDED",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    color: "#000000",
+    width: 270,
+    height: 40,
+  },
+});
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from "react";
-import { Text, View, TextInput, Button } from "react-native";
-import styles from "../styles/styles";
+import { Text, View, TextInput, Button, StyleSheet } from "react-native";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { firebase } from "../backend/firebase-config";
 import UserContext from "../context/user-context";
@@ -35,7 +34,7 @@ export default function SignUpForm({ navigation }) {
   };
 
   return (
-    <KeyboardAvoidingView style={styles.container}>
+    <KeyboardAvoidingView style={styles.signupContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
@@ -51,9 +50,49 @@ export default function SignUpForm({ navigation }) {
           secureTextEntry
         />
         <TouchableOpacity onPress={handleSignup} style={styles.button}>
-          <Text style={styles.button}>SIGN UP</Text>
+          <Text style={styles.buttonText}>SIGN UP</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  signupContainer: {
+    flex: 1,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 22,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#ffffff",
+    paddingHorizontal: 75,
+    paddingVertical: 7,
+    borderRadius: 5,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 18,
+    width: 120,
+    textAlign: 'center'
+  },
+  input: {
+    backgroundColor: '#EDEDED',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    color: '#000000',
+    width: 270, 
+    height: 40, 
+  }
+});
