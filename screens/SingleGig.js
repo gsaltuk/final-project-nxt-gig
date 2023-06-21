51b277b2d1db97b970383378c605099cce811492
alexnorniella <alexnorniella@yahoo.co.uk>
2023-06-21 10:54:38 +0100

    new changes

--- screens/SingleGig.js
+++ screens/SingleGig.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { Linking } from "react-native";
+import { getAuth } from "firebase/auth";
+import { firebase } from "../backend/firebase-config";
+import useLoggedInUser from "../backend/firebase-auth";
 
 const SingleGig = () => {
+  const [isFavouriteGig, setisFavouriteGig] = useState(false);
+
+  const loggedUser = useLoggedInUser();
+
+  console.log(loggedUser, "auth in the single gig");
   const route = useRoute();
   const { gig } = route.params || {};
-  console.log(gig,'gig')
-  const navigation = useNavigation();
-
-  const handleGetTickets = () => {
-    navigation.navigate("GetTickets", { gig });
-  };
+  const handleFavouriteGig = async () => {};
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={{ uri: gig.imageURL }} style={styles.image} resizeMode="contain" />
+        <Image
+          source={{ uri: gig.imageURL }}
+          style={styles.image}
+          resizeMode="contain"
+        />
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Interested</Text>
+        <TouchableOpacity
+          onPress={handleFavouriteGig}
+          style={[
+            styles.addFavouriteGig,
+            isFavouriteGig
+              ? { backgroundColor: red }
+              : { backgroundColor: "#fc038c" },
+          ]}
+        >
+          <Text style={styles.addFavouriteGig}>
+            {isFavouriteGig ? "Remove from favourites" : "Add to favourites"}
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Share</Text>
@@ -66,6 +83,13 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   button: {
+    backgroundColor: "#fc038c",
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 4,
+    marginHorizontal: 5,
+  },
+  addFavouriteGig: {
     backgroundColor: "#fc038c",
     paddingVertical: 10,
     paddingHorizontal: 16,
